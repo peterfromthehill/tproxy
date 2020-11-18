efc371e39d6e8d3621e14493fe7cce016a76967b
peterfromthehill <peter@1qay.net>
2020-11-18 22:35:36 +0100

    use spaces instead of tabs

--- proxy_test.go
+++ proxy_test.go
@@ -5,19 +5,19 @@ import "net/http"
 import "reflect"
 
 func TestCopyHeader(t *testing.T) {
-	var src = http.Header{}
-	var dst = http.Header{}
-	src.Add("host", "localhost")
-	src.Add("Connection", "keep-alive")
-	src.Add("Keep-alive", "10")
+    var src = http.Header{}
+    var dst = http.Header{}
+    src.Add("host", "localhost")
+    src.Add("Connection", "keep-alive")
+    src.Add("Keep-alive", "10")
 
-	var assertdst = http.Header{}
-	assertdst.Add("host", "localhost")
-	assertdst.Add("Connection", "close")
+    var assertdst = http.Header{}
+    assertdst.Add("host", "localhost")
+    assertdst.Add("Connection", "close")
 
-	copyHeader(dst, src)
+    copyHeader(dst, src)
 
-	if !reflect.DeepEqual(dst, assertdst) {
-		t.Errorf("dst not equal asserdst: %s <=> %s", dst, assertdst)
-	}
+    if !reflect.DeepEqual(dst, assertdst) {
+        t.Errorf("dst not equal asserdst: %s <=> %s", dst, assertdst)
+    }
 }
